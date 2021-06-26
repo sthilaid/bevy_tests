a3bfb5cff0b3c25ee5683d4cb73c540cccf254cf
David St-Hilaire <sthilaire.david@gmail.com>
2021-06-26 12:25:29 -0400

    added q to quit and started modding the base scene

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use bevy::prelude::*;
+use bevy::{
+    app::AppExit,
+    input::{keyboard::KeyCode, Input},
+    prelude::*,
+};
 
 pub struct HelloPlugin;
 
@@ -37,40 +41,55 @@ impl Plugin for HelloPlugin {
             .add_system(greet_people.system());
     }
 }
-struct Cube;
-fn setup_scene(
-    mut commands: Commands,
+
+struct Star;
+fn spawn_star(
+    commands: &mut Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
+    pos: &Vec3,
 ) {
-    // plane
-    commands.spawn_bundle(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
-        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
-        ..Default::default()
-    });
-    // cube
     commands
         .spawn_bundle(PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+            mesh: meshes.add(Mesh::from(shape::Icosphere {
+                radius: 0.5,
+                subdivisions: 2,
+            })),
             material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-            transform: Transform::from_xyz(0.0, 0.5, 0.0),
+            transform: Transform::from_translation(*pos),
             ..Default::default()
         })
-        .insert(Cube);
-    // light
-    commands.spawn_bundle(LightBundle {
-        transform: Transform::from_xyz(4.0, 8.0, 4.0),
-        ..Default::default()
-    });
-    // camera
-    commands.spawn_bundle(PerspectiveCameraBundle {
-        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
-        ..Default::default()
-    });
+        .insert(Star);
 }
 
-fn update_scene(time: Res<Time>, mut query: Query<(&Cube, &mut Transform)>) {
+fn setup_scene(
+    mut commands: Commands,
+    meshes: ResMut<Assets<Mesh>>,
+    materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // // plane
+    // commands.spawn_bundle(PbrBundle {
+    //     mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
+    //     material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+    //     ..Default::default()
+    // });
+    spawn_star(&mut commands, meshes, materials, &Vec3::new(0.0, 0.5, -1.0));
+
+    // light
+    // commands.spawn_bundle(LightBundle {
+    //     transform: Transform::from_xyz(4.0, 8.0, 4.0),
+    //     ..Default::default()
+    // });
+    // camera
+    // commands.spawn_bundle(PerspectiveCameraBundle {
+    //     transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+    //     ..Default::default()
+    // });
+
+    commands.spawn_bundle(OrthographicCameraBundle::new_3d());
+}
+
+fn update_scene(time: Res<Time>, mut query: Query<(&Star, &mut Transform)>) {
     for (_, mut pbr) in query.iter_mut() {
         let delta_y = (f32::cos(2.0 * time.seconds_since_startup() as f32) + 1.0) * 0.5;
         pbr.translation.y = 0.5 + delta_y;
@@ -85,10 +104,24 @@ impl Plugin for ScenePlugin {
     }
 }
 
+fn input_system(mut exit: EventWriter<AppExit>, keyboard_input: Res<Input<KeyCode>>) {
+    if keyboard_input.pressed(KeyCode::Q) {
+        exit.send(AppExit);
+    }
+}
+
+pub struct GamePlugin;
+impl Plugin for GamePlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app.add_system(input_system.system());
+    }
+}
+
 fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
         .add_plugin(HelloPlugin)
         .add_plugin(ScenePlugin)
+        .add_plugin(GamePlugin)
         .run();
 }
